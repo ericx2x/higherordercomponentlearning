84d22ecc66f5120224a8e69e810f4a4dd6444bd8
Eric Lima <ericjlima@gmail.com>
2018-07-17 18:22:09 -0400

    made a higher ordered component

--- src/HOC/stylesWrapper.js
+++ src/HOC/stylesWrapper.js
@@ -1,7 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import commonStyles from './../styles/commonStyles';
+
+const translateProps = (props) => {
+    console.log(props);
+    let _styles = {...commonStyles.default};
+    if(props.disable){
+        _styles = {..._styles, ...commonStyles.disable};
+    }
+    const newProps = {...props, styles:_styles}
+    return newProps;
+    
+}
 
 
 export default (WrappedComponent) => {
-    
+    return function wrapperRender(args){
+        return WrappedComponent(translateProps(args));
+    }
 }
--- src/components/ButtonOne.js
+++ src/components/ButtonOne.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import stylesWrapper from './../HOC/stylesWrapper';
 
 const ButtonOne = (props) => {
-    let _styles = {...styles.default};
-    if (props.disable){
-        _styles = {...styles, ...styles.disable};
-        console.log(_styles);
-    }
+    // let _styles = {...styles.default};
+    // if (props.disable){
+    //     _styles = {...styles, ...styles.disable};
+    //     console.log(_styles);
+    // }
     return (
-        <button style={_styles}>I am ButtonOne</button>
+        <button style={props.styles}>I am ButtonOne</button>
     )
 }
 
-export default ButtonOne;
+export default stylesWrapper(ButtonOne);
